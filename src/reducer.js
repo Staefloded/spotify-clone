ac3e16bc3626f430efcc7ceca9c75f40384acded
staefloded <michaeleleyowo29@gmail.com>
2021-08-26 10:43:15 +0100

    removed token

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,7 @@ export const initialState = {
   playlists: [],
   item: null,
   //   Remove after finished developing
-  token:
-    "BQDRGDtCsnG3qnbMbiUXzj97OwV20NEA_zN1lG7VGNVklSVtT5vvtjx54Bhj-TfUKY48Dce1msga038fY7DGIQIUAaIv1f5OUykWLyOx36MN-tknMbZJGp-rk0FybKjA8waslgsLSsU_aog1fZMh2greqS6Mu3rpAjMMWmHNCVxb8i9wuQ",
+  token: null,
 };
 
 const reducer = (state, action) => {
